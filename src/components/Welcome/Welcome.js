65aa94f1c59eb235398310f7e77ba27d1fa59d54
Igor Sachivka <juicyigor@icloud.com>
2015-10-26 13:57:46 +0300

    markup welcome component

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -9,12 +9,27 @@ export default class Welcome extends Component {
     const icons = require('../Styles/icons.less');
     return (
       <div className={styles.welcome}>
-        <h1 className={styles.h1}>{welcome.h1}</h1>
-        <div className={styles.company}>
-          {welcome.from} <a className={styles.a} href="">{welcome.companyName}</a>
+        <div className={styles.container}>
+          <div className={styles.containerRow}>
+            <div className={styles.containerCell}>
+              <div className={styles.containerRight}>
+                <div className={icons.main}></div>
+              </div>
+              <div className={styles.containerLeft}>
+                <h1 className={styles.h1}>{welcome.h1}</h1>
+                <div className={styles.company}>
+                  {welcome.from}
+                  <a className={styles.a} href="">{welcome.companyName}</a>
+                </div>
+                <p className={styles.p}>{welcome.text}</p>
+                <button className={styles.button}>
+                  <i className={icons.go}></i>
+                  {welcome.button}
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
-        <p className={styles.p}>{welcome.text}</p>
-        <button className={styles.button}><i className={icons.go}></i>{welcome.button}</button>
       </div>
     );
   }
