f56ed450003fb34d486fc0b760f8534568ac9023
Igor Sachivka <juicyigor@icloud.com>
2015-11-09 18:56:40 +0300

    html ga

--- src/helpers/Html.js
+++ src/helpers/Html.js
@@ -19,6 +19,17 @@ export default class Html extends Component {
     store: PropTypes.object
   }
 
+  trackingCode() {
+    return ({__html:
+      `(function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=` +
+      `function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;` +
+      `e=o.createElement(i);r=o.getElementsByTagName(i)[0];` +
+      `e.src='https://www.google-analytics.com/analytics.js';` +
+      `r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));` +
+      `ga('create','UA-69854681-1','auto');ga('send','pageview');`,
+    });
+  }
+
   render() {
     const {assets, component, store} = this.props;
     const content = component ? ReactDOM.renderToString(component) : '';
@@ -46,6 +57,7 @@ export default class Html extends Component {
         <body>
           <section id="content" style={{height: '100%'}} dangerouslySetInnerHTML={{__html: content}} />
           <script dangerouslySetInnerHTML={{__html: `window.__data=${serialize(store.getState())};`}} />
+          <script dangerouslySetInnerHTML={this.trackingCode()} />
           <script src={assets.javascript.main}/>
         </body>
       </html>
