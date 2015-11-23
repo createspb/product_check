cf95739c2aaa843e42c5edcf429535a719dd60df
Igor Sachivka <juicyigor@icloud.com>
2015-11-23 18:02:45 +0300

    sending messages

--- api/actions/send.js
+++ api/actions/send.js
@@ -1,5 +1,36 @@
+import aws from 'aws-sdk';
+
 export default function send(req) {
   return new Promise((resolve, reject) => {
-    resolve(true);
+    aws.config.update({
+      accessKeyId: process.env.SES_KEY_ID,
+      secretAccessKey: process.env.SES_ACCESS_KEY,
+    });
+    aws.config.update({region: 'eu-west-1'});
+    const ses = new aws.SES({apiVersion: '2010-12-01'});
+    const to = ['account@createdigital.me'];
+    const from = 'robot@createdigital.me';
+    ses.sendEmail({
+      Source: from,
+      Destination: { ToAddresses: to },
+      Message: {
+        Subject: {
+          Data: 'Сообщение из АудитаПродукта',
+        },
+        Body: {
+          Text: {
+            Data: `E-mail: ${req.body.email},
+            Text: ${req.body.text}`
+          }
+        }
+      }
+    }, (err, data) => {
+      if (err) {
+        console.log(err);
+        reject(true);
+      } else {
+        resolve(data);
+      }
+    });
   });
 }
