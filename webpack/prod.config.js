ed10b7bcd477366cdd82a5fc6722980c367fc0bb
Igor Sachivka <juicyigor@icloud.com>
2015-12-01 16:15:37 +0300

    fix

--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -13,6 +13,7 @@ var assetsPath = path.join(__dirname, relativeAssetsPath);
 var WebpackIsomorphicToolsPlugin = require('webpack-isomorphic-tools/plugin');
 var webpackIsomorphicToolsPlugin = new WebpackIsomorphicToolsPlugin(require('./webpack-isomorphic-tools'));
 
+
 module.exports = {
   devtool: 'source-map',
   context: path.resolve(__dirname, '..'),
